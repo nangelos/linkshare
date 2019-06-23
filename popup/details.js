14cce73e0069ed6b03861ab4d71a02af119b2cfd
Taylor Thompson <jamestthompson3@gmail.com>
2019-06-24 00:33:38 +0300

    Accesses chats from storage

--- popup/details.js
+++ popup/details.js
@@ -2,7 +2,6 @@ const getCurrentWindowTabs = () => {
   // return browser.tabs.query({ currentWindow: true });
   return browser.tabs.query({ active: true })
 }
-
 const listTabs = () => {
   getCurrentWindowTabs().then(tab => {
     console.log('here is the tab: ', tab)
@@ -13,34 +12,35 @@ const listTabs = () => {
     tabsList.textContent = ''
 
     //Tab title
-    let tabTitle = document.createElement('h4')
+    const tabTitle = document.createElement('h4')
     tabTitle.textContent = activeTab.title || activeTab.id
 
     //Input box
-    let textInput = document.createElement('textarea')
+    const textInput = document.createElement('textarea')
     textInput.className = 'detail-box'
     textInput.name = 'message'
     textInput.rows = '4'
     textInput.placeholder = 'Add description (optional)'
 
-    let chatName = document.createElement('select')
+    const chatName = document.createElement('select')
     chatName.id = 'chat-name'
-    let options = [
-      { text: 'Option 1', value: 'option1' },
-      { text: 'Option 2', value: 'option2' },
-    ]
+
+    const options = browser.storage.sync.get('chats')
     const makeOptions = arr => {
-      arr.map(opt => {
+      const { chats } = arr
+      const existingChats = Array.isArray(chats) ? chats : []
+      console.log(existingChats)
+      existingChats.map(opt => {
         let option = document.createElement('option')
         option.appendChild(document.createTextNode(opt.text))
         option.value = opt.value
         chatName.appendChild(option)
       })
     }
-    makeOptions(options)
+    options.then(makeOptions)
 
     //Send Button
-    let sendButton = document.createElement('input')
+    const sendButton = document.createElement('input')
     sendButton.className = 'button send'
     sendButton.type = 'submit'
     sendButton.value = 'Send URL'
@@ -48,7 +48,7 @@ const listTabs = () => {
     const chatButton = document.createElement('input')
     chatButton.className = 'button chat'
     chatButton.type = 'button'
-    chatButton.value = 'Add Chat'
+    chatButton.value = 'Edit Chats'
     chatButton.addEventListener('click', () => {
       const form = document.querySelector('form')
       form.style.display = 'flex'
@@ -72,11 +72,38 @@ function saveChat(e) {
   e.preventDefault()
   const chatId = document.querySelector('#chatId')
   const chatName = document.querySelector('#chatName')
-  browser.storage.sync.set({
-    chatId,
-    chatName,
-  })
+  const options = browser.storage.sync.get('chats')
+  options.then(appendChats)
+
+  function appendChats(result) {
+    const { chats } = result
+    const existingChats = Array.isArray(chats) ? chats : []
+    browser.storage.sync
+      .set({
+        chats: [
+          ...existingChats,
+          { text: chatName.value, value: chatId.value },
+        ],
+      })
+      .then(listTabs)
+      .then(hideForm)
+  }
+}
+
+function clearChats(e) {
+  e.preventDefault()
+  browser.storage.sync
+    .clear()
+    .then(listTabs)
+    .then(hideForm)
+}
+
+function hideForm() {
+  document.querySelector('form').style.display = 'none'
+  document.querySelector('#chatId').value = ''
+  document.querySelector('#chatName').value = ''
 }
 
 document.addEventListener('DOMContentLoaded', listTabs)
 document.querySelector('form').addEventListener('submit', saveChat)
+document.querySelector('#clear').addEventListener('click', clearChats)
