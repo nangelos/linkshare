3b7da6b744396ffbd5bd79aa0974f73ba451af10
nangelos <nicksangelos@gmail.com>
2019-06-23 01:55:54 -0600

    [background] isolated URL and now need to send it in message

--- background.js
+++ background.js
@@ -1,24 +1,48 @@
-browser.webNavigation.onHistoryStateUpdated.addListener(
-  details => {
-    broser.tabs.executeScript(null, { file: 'pagetotelegram.js' });
-  } /*, {
-  url: [{originAndPathMatches: '^.+://dev.to/.+/.+$'}]
-}*/
-);
+// browser.webNavigation.onHistoryStateUpdated.addListener(
+//   details => {
+//     broser.tabs.executeScript(null, { file: 'pagetotelegram.js' });
+//   }
+// );
 
-const handleMessage = (message, sender, sendResponse) => {
-  if (message.url) {
-    sendToTelegram(message.url, sendResponse);
-    return true;
-  }
-};
-browser.runtime.onMessage.addListener(handleMessage);
+// const openPage = () => {
+//   browser.tabs.create({
+//     url: `http://www.espn.com`,
+//   });
+// };
+console.log('In the background.js script');
 
-const sendToTelegram = (url, sendResponse) => {
-  let xhr = new XMLHttpRequest();
-  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-    sendResponse();
-  }
+const sendLink = () => {
+  let linkUrl;
+  const querying = browser.tabs.query({
+    active: true,
+  });
+  // querying.then(tabs => {
+  //   for (tab in tabs) {
+  //     console.log(tabs[tab]);
+  //   }
+  // });
+  querying.then(res => {
+    linkUrl = res[0].url;
+    console.log(linkUrl);
+    //now need to send along the linkUrl to Telegram
+  });
 };
-xhr.open('GET', `https://getpocket.com/save?url=${url}`, true);
-xhr.send();
+
+browser.browserAction.onClicked.addListener(sendLink);
+
+// const handleMessage = (message, sender, sendResponse) => {
+//   if (message.url) {
+//     sendToTelegram(message.url, sendResponse);
+//     return true;
+//   }
+// };
+// browser.runtime.onMessage.addListener(handleMessage);
+
+// const sendToTelegram = (url, sendResponse) => {
+//   let xhr = new XMLHttpRequest();
+//   if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+//     sendResponse();
+//   }
+// };
+// xhr.open('GET', `https://getpocket.com/save?url=${url}`, true);
+// xhr.send();
